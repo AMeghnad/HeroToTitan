8ad6872d774cb87f58ceafd13f0d626c66e9dae7
AMeghnad <anshumanmeghnad@gmail.com>
2017-12-18 16:04:12 +1100

    SuperMarioGalaxy and GGL uploads

---
+++ Assets/~SuperMarioGalaxy/Scripts/Camera/CameraFollowsSphere.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperMarioGalaxy
+{
+    public class CameraFollowsSphere : MonoBehaviour
+    {
+        public GravityBody target;
+        public Vector3 offset = new Vector3(0, 0, -20);
+        public float speed = 5f;
+        public float sphereRadius = 5f;
+
+        private Vector3 centrePos; // Camera centre
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(centrePos, sphereRadius);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            centrePos = target.transform.position;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            // Is there a valid target?
+            if (target != null)
+            {
+                float distance = Vector3.Distance(centrePos, target.transform.position);
+                // Is target's distance further from centre of sphere
+                if (distance > sphereRadius)
+                {
+                    // Move centre to new target pos
+                    centrePos = Vector3.Lerp(centrePos, target.transform.position, speed * Time.deltaTime);
+                }
+                Vector3 worldOffset = Quaternion.LookRotation(target.Gravity) * offset;
+                transform.position = Vector3.Lerp(transform.position, centrePos + worldOffset, speed * Time.deltaTime);
+                transform.LookAt(target.transform, transform.up);
+            }
+        }
+    }
+}
---
+++ Assets/~SuperMarioGalaxy/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GGL;
+
+namespace SuperMarioGalaxy
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Player : MonoBehaviour
+    {
+        public float speed = 20f;
+        public float groundDistance = 3f;
+
+        private Rigidbody rigid;
+        private GravityNormal body;
+        private Vector3 force;
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + force);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + -transform.up * groundDistance);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+            body = GetComponent<GravityNormal>();
+        }
+
+        public void Move(float inputH, float inputV)
+        {
+            Vector3 localForce = new Vector3(inputH, 0, inputV);
+            force = Quaternion.LookRotation(Camera.main.transform.up, -body.Gravity) * localForce;
+            GizmosGL.AddLine(transform.position, transform.position + force, 0.3f, 0.3f, Color.blue, Color.blue);
+            rigid.AddForce(force * speed);
+
+            if (body.isGrounded)
+            {
+                Vector3 groundHitPos = body.HitPoint;
+                GizmosGL.AddSphere(groundHitPos, 1f, null, Color.red);
+                float distanceToGround = Vector3.Distance(transform.position, groundHitPos);
+
+                GizmosGL.AddSphere(groundHitPos + body.Gravity.normalized * distanceToGround, 1f, null, Color.blue);
+
+                if (distanceToGround > groundDistance)
+                {
+                    transform.position = groundHitPos + body.Gravity.normalized * distanceToGround;
+                }
+            }
+        }
+    }
+}
